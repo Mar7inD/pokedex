4412d2405c04cca76f682d5bea2e34cb2b1d304f
Mar7inD <Dotev32@gmail.com>
2024-03-31 12:52:27 +0200

    Header is added

--- src/Components/header.js
+++ src/Components/header.js
@@ -5,9 +5,18 @@ const header = () => {
 
     return(
         <>
+            <div class="element-with-top-shadow"></div>
             <nav>
-                <Link to="/" className="link">Pokedex</Link>
-                <Link to="/about" className="link">About</Link>
+                
+
+                <Link to="/" className="link">
+                    <p className='pokedex-text'>Pokédex</p>
+                    <span className="pokedex-line"></span>
+                </Link>
+                <Link to="/about" className="link">
+                    <p className='about-text'>About</p>
+                    <span className="about-line"></span>
+                </Link>
             </nav>
         </>
     )
